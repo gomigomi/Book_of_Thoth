888a35b813c47dd5317e358f32e8b2c4a1c01c27
gomgom <snut301@naver.com>
2015-06-17 00:10:07 +0900

    button event FIN

--- WebContent/js/custom.js
+++ WebContent/js/custom.js
@@ -2,7 +2,6 @@ $(function() {
 	$('.prebtn').click(function(){
 		var thisNum=parseInt($(this).parents('div').attr('id'));
 		var preNum=thisNum-1;
-		var preid=preNum.toString();
 		if(thisNum==1){
 			alert('첫 페이지 입니다.');
 		}else{
@@ -11,14 +10,81 @@ $(function() {
 		}
 	});
 	$('.nextbtn').click(function(){
-		var thisNum=parseInt($(this).parents('div').attr('id'));
-		var nextNum=thisNum+1;
-		var nextid=nextNum.toString();
+		var thisNum=$(this).parents('div').attr('id');
+		var nextNum=parseInt(thisNum)+1;
 		$('#'+thisNum).hide();
-		if(nextid==23){
+		if(thisNum==22){
 			$('#goodEnding').show();
+		}else if(thisNum==6){
+			$('#selection7').show();
+		}else if(thisNum==9){
+			$('#selection10').show();
+		}else if(thisNum==14){
+			$('#selection15').show();
+		}else if(thisNum==16){
+			$('#selection17').show();
+		}else if(thisNum==20){
+			$('#selection21').show();
+		}else if(thisNum=='15-1' || thisNum=='15-2'){
+			$('#selection15').show();
+		}else if(thisNum=='7-1' || thisNum=='7-2'|| thisNum=='10-1'|| thisNum=='21-1'){
+			$('#badEnding').show();
 		}else{
 			$('#'+nextNum).show();
 		}
 	});
+	
+	$('#btn7').click(function(){
+		$('#selection7').hide();
+		$('#7').show();
+	})	
+	$('#btn7-1').click(function(){
+		$('#selection7').hide();
+		$('#7-1').show();
+	})	
+	$('#btn7-2').click(function(){
+		$('#selection7').hide();
+		$('#7-2').show();
+	})	
+	$('#btn10').click(function(){
+		$('#selection10').hide();
+		$('#10').show();
+	})	
+	$('#btn10-1').click(function(){
+		$('#selection10').hide();
+		$('#10-1').show();
+	})	
+	$('#btn15-1').click(function(){
+		$('#selection15').hide();
+		$('#15-1').show();
+	})	
+	$('#btn15-2').click(function(){
+		$('#selection15').hide();
+		$('#15-2').show();
+	})	
+	$('#btn15').click(function(){
+		$('#selection15').hide();
+		$('#15').show();
+	})
+	$('#btn17').click(function(){
+		$('#selection17').hide();
+		$('#17').show();
+	})	
+	$('#btn17-1').click(function(){
+		$('#selection17').hide();
+		$('#17-1').show();
+	})	
+	$('#btn21').click(function(){
+		$('#selection21').hide();
+		$('#21').show();
+	})	
+	$('#btn21-1').click(function(){
+		$('#selection21').hide();
+		$('#21-1').show();
+	})
+	
+	$('#to1').click(function(){
+		$('#badEnding').hide();
+		$('#1').show();
+	})
 });
