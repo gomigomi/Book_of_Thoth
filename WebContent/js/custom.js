7da9aeea7c0bfa810a08f82493303804be41c95b
gomgom <snut301@naver.com>
2015-06-17 01:50:11 +0900

    순서 맞추기 게임

--- WebContent/js/custom.js
+++ WebContent/js/custom.js
@@ -1,3 +1,5 @@
+var x=[];
+
 $(function() {
 	$('.prebtn').click(function(){
 		var thisNum=parseInt($(this).parents('div').attr('id'));
@@ -27,7 +29,7 @@ $(function() {
 			$('#selection21').show();
 		}else if(thisNum=='15-1' || thisNum=='15-2'){
 			$('#selection15').show();
-		}else if(thisNum=='7-1' || thisNum=='7-2'|| thisNum=='10-1'|| thisNum=='21-1'){
+		}else if(thisNum=='7-1' || thisNum=='7-2'|| thisNum=='10-1'|| thisNum=='17-1'|| thisNum=='21-1'){
 			$('#badEnding').show();
 		}else{
 			$('#'+nextNum).show();
@@ -87,4 +89,64 @@ $(function() {
 		$('#badEnding').hide();
 		$('#1').show();
 	})
+	
+	$('#btn_1').click(function(){
+		pushX('1');
+		checkLength();
+	})
+	$('#btn_2').click(function(){
+		pushX('2');
+		checkLength();
+	})
+	$('#btn_3').click(function(){
+		pushX('3');
+		checkLength();
+	})
+	$('#btn_4').click(function(){
+		pushX('4');
+		checkLength();
+	})
+	$('#btn_5').click(function(){
+		pushX('5');
+		checkLength();
+	})
+	$('#btn_6').click(function(){
+		pushX('6');
+		checkLength();
+	})
 });
+
+function checkLength(){
+	if(x.length==6){
+		if(x[0]=='1' && x[1]=='2' && x[2]=='3' && x[3]=='4' && x[4]=='5'&& x[5]=='6'){
+			$('#selection17').hide();
+			$('#17').show();
+			$('#show').empty();
+			x=[];
+		}else{
+			$('#selection17').hide();
+			$('#17-1').show();
+			$('#show').empty();
+			x=[];
+		}
+	}
+}
+
+function pushX(input){
+	x.push(input);
+	var kor;
+	if(input=='1'){
+		kor="철";
+	}else if(input=='2'){
+		kor="청동";
+	}else if(input=='3'){
+		kor="단풍나무";
+	}else if(input=='4'){
+		kor="상아와 흑단";
+	}else if(input=='5'){
+		kor="은";
+	}else if(input=='6'){
+		kor="금";
+	}
+	$('#show').append(kor);
+}
